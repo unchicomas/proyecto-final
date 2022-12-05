9d6d7b53065313c78ad39c5e4afbe1dd4780b861
unchicomas <vazquezbraianleonel@gmail.com>
2022-12-05 18:26:25 -0300

    ultimos cambios de mi proyecto

---
+++ ejemplo/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from ejemplo.models import Familiar
+from ejemplo.forms import Buscar # <--- NUEVO IMPORT
+from django.views import View # <-- NUEVO IMPORT 
+
+def index(request):
+    return render(request, "ejemplo/saludar.html")
+
+
+def saludar_a(request, nombre):
+    return render(request ,
+     'ejemplo/saludar_a.html',
+    {"nombre": nombre}    #contexto 
+    )
+
+def sumar(request, a, b):
+    return render (request,
+    'ejemplo/sumar.html',
+    {"a":a,
+    "b": b,
+    "resultado": a + b
+    }
+    )
+
+def monstrar_familiares(request):
+    lista_familiares = Familiar.objects.all()
+    return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares},
+  )
+
+class BuscarFamiliar(View):
+    form_class = Buscar
+    template_name = 'ejemplo/buscar.html'
+    initial = {"nombre":""}
+    def get(self, request):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form':form})
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data.get("nombre")
+            lista_familiares = Familiar.objects.filter(nombre__icontains=nombre).all() 
+            form = self.form_class(initial=self.initial)
+            return render(request, self.template_name, {'form':form, 
+                                                        'lista_familiares':lista_familiares})
+        return render(request, self.template_name, {"form": form},
+    )       
---
+++ seed_data.py
@@ -0,0 +1,9 @@
+from ejemplo.models import Familiar
+
+Familiar(nombre="Leonel", direccion="Chaco 76", numero_pasaporte=233122).save()
+Familiar(nombre="Jose", direccion="Serrano 4343", numero_pasaporte=30203421).save()
+Familiar(nombre="Damian", direccion="Serrano 4343", numero_pasaporte=43213255).save()
+Familiar(nombre="Elida", direccion="Serrano 4343", numero_pasaporte=22844334).save()
+print("Se cargo con éxito los usuarios de pruebas")
+
+
