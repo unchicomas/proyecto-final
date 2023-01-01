cbae6530fd608593864fd90570863dee1000c1a9
unchicomas <vazquezbraianleonel@gmail.com>
2023-01-01 10:28:28 -0300

    primer commit de mi proyecto

--- Project/urls.py
+++ Project/urls.py
@@ -1,4 +1,4 @@
-"""Project URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,28 +13,54 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import (ListView, CreateView, DeleteView, UpdateView, TemplateView)
-
-from ejemplo.views import (index, saludar_a, sumar, monstrar_familiares,
-                                BuscarFamiliar, AltaFamiliar, ActualizarFamiliar,FamiliarList, FamiliarCrear, FamiliarBorrar, FamiliarActualizar, )
-
+from django.views.generic import TemplateView
+from ejemplo.views import (index, saludar_a, sumar, 
+                            buscar, monstrar_familiares,
+                            BuscarFamiliar, AltaFamiliar,
+                            ActualizarFamiliar, BorrarFamiliar,
+                            FamiliarList, FamiliarCrear, FamiliarBorrar, FamiliarActualizar, FamiliarDetalle)
+from ejemplo_dos.views import (index, PostDetalle, PostListar, 
+                               PostCrear, PostBorrar, PostActualizar,
+                               UserSignUp, UserLogin, UserLogout, 
+                               AvatarActualizar, UserActualizar, MensajeCrear, MensajeListar, MensajeDetalle )
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludar/', index), # ESTA ES LA NUEVA FUNCTION 
-    path('saludar-a/<nombre>', saludar_a),
+    path('saludar/', index),
+    path('saludar-a/<nombre>/', saludar_a),
     path('sumar/<int:a>/<int:b>/', sumar),
-    path('mi-familia/', monstrar_familiares), # nueva vista
-    path('mi-familia/buscar', BuscarFamiliar.as_view()), # NUEVA RUTA PARA BUSCAR FAMILIAR
+    path('buscar/',buscar),
+    path('mi-familia/', monstrar_familiares),
+    path('mi-familia/buscar', BuscarFamiliar.as_view()),
     path('mi-familia/alta', AltaFamiliar.as_view()),
     path('mi-familia/actualizar/<int:pk>', ActualizarFamiliar.as_view()),
+    path('mi-familia/borrar/<int:pk>', BorrarFamiliar.as_view()),
     path('panel-familia/', FamiliarList.as_view()),
     path('panel-familia/crear', FamiliarCrear.as_view()),
     path('panel-familia/<int:pk>/borrar', FamiliarBorrar.as_view()),
     path('panel-familia/<int:pk>/actualizar', FamiliarActualizar.as_view()),
-    path('ejemplo-dos/', index),
-    
-
+    path('panel-familia/<int:pk>/detalle', FamiliarDetalle.as_view()),
+    path('success_updated_message/',TemplateView.as_view(template_name="ejemplo/success_updated_message.html")),
+    path('ejemplo-dos/', index, name="ejemplo-dos-index"),
+    path('ejemplo-dos/<int:pk>/detalle/', PostDetalle.as_view(), name="ejemplo-dos-detalle"),
+    path('ejemplo-dos/listar/', PostListar.as_view(), name="ejemplo-dos-listar"),
+    path('ejemplo-dos/crear/', staff_member_required(PostCrear.as_view()), name="ejemplo-dos-crear"),
+    path('ejemplo-dos/<int:pk>/borrar/', staff_member_required(PostBorrar.as_view()), name="ejemplo-dos-borrar"),
+    path('ejemplo-dos/<int:pk>/actualizar/', staff_member_required(PostActualizar.as_view()), name="ejemplo-dos-actualizar"),
+    path('ejemplo-dos/signup/', UserSignUp.as_view(), name ="ejemplo-dos-signup"),
+    path('ejemplo-dos/login/', UserLogin.as_view(), name= "ejemplo-dos-login"),
+    path('ejemplo-dos/logout/', UserLogout.as_view(), name="ejemplo-dos-logout"),
+    path('ejemplo-dos/avatars/<int:pk>/actualizar/', AvatarActualizar.as_view(), name="ejemplo-dos-avatars-actualizar"),
+    path('ejemplo-dos/users/<int:pk>/actualizar/', UserActualizar.as_view(), name="ejemplo-dos-users-actualizar"),
+    path('ejemplo-dos/mensajes/crear/', MensajeCrear.as_view(), name="ejemplo-dos-mensajes-crear"),
+    path('ejemplo-dos/mensajes/<int:pk>/detalle/', MensajeDetalle.as_view(), name="ejemplo-dos-mensajes-detalle"),
+    path('ejemplo-dos/mensajes/listar/', MensajeListar.as_view(), name="ejemplo-dos-mensajes-listar"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,40 +1,55 @@
 from django.shortcuts import render, get_object_or_404
 from ejemplo.models import Familiar
-from ejemplo.forms import Buscar, FamiliarForm # <--- NUEVO IMPORT
-from django.views import View # <-- NUEVO IMPORT 
-from django.views.generic import (ListView, CreateView, DeleteView, UpdateView)
+from ejemplo.forms import Buscar, FamiliarForm
+from django.views import View
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 
 def index(request):
     return render(request, "ejemplo/saludar.html")
 
 
 def saludar_a(request, nombre):
-    return render(request ,
-     'ejemplo/saludar_a.html',
-    {"nombre": nombre}    #contexto 
+    return render(request, 
+    'ejemplo/saludar_a.html',
+    {"nombre": nombre}
     )
+
 
 def sumar(request, a, b):
     return render (request,
-    'ejemplo/sumar.html',
-    {"a":a,
-    "b": b,
-    "resultado": a + b
+    'ejemplo/sumar.html', 
+    {"a": a,
+     "b": b,
+     "resultado": a + b
     }
     )
 
+
+def buscar(request):
+    lista_de_nombre = ["German", "Daniel", "Romero", "Alvaro"]
+    query = request.GET['q']
+    if query in lista_de_nombre:
+        indice_de_resultado = lista_de_nombre.index(query)
+        resultado = lista_de_nombre[indice_de_resultado]
+    else:
+        resultado = "no hay match"
+    return render(request, 'ejemplo/buscar.html', {"resultado": resultado})
+
+
 def monstrar_familiares(request):
-    lista_familiares = Familiar.objects.all()
-    return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares},
-  )
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
+
 
 class BuscarFamiliar(View):
     form_class = Buscar
     template_name = 'ejemplo/buscar.html'
     initial = {"nombre":""}
+    
     def get(self, request):
         form = self.form_class(initial=self.initial)
         return render(request, self.template_name, {'form':form})
+    
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
@@ -43,10 +58,7 @@ class BuscarFamiliar(View):
             form = self.form_class(initial=self.initial)
             return render(request, self.template_name, {'form':form, 
                                                         'lista_familiares':lista_familiares})
-        return render(request, self.template_name, {"form": form},
-    )
-
-
+        return render(request, self.template_name, {"form": form})
 
 class AltaFamiliar(View):
 
@@ -62,7 +74,7 @@ class AltaFamiliar(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
-            msg_exito = f"Se cargo con éxito el familiar {form.cleaned_data.get('nombre')}"
+            msg_exito = f"se cargo con éxito el familiar {form.cleaned_data.get('nombre')}"
             form = self.form_class(initial=self.initial)
             return render(request, self.template_name, {'form':form, 
                                                         'msg_exito': msg_exito})
@@ -87,7 +99,7 @@ class ActualizarFamiliar(View):
       form = self.form_class(request.POST ,instance=familiar)
       if form.is_valid():
           form.save()
-          msg_exito = f"Se actualizó con éxito el familiar {form.cleaned_data.get('nombre')}"
+          msg_exito = f"se actualizó con éxito el familiar {form.cleaned_data.get('nombre')}"
           form = self.form_class(initial=self.initial)
           return render(request, self.template_name, {'form':form, 
                                                       'familiar': familiar,
@@ -96,32 +108,32 @@ class ActualizarFamiliar(View):
       return render(request, self.template_name, {"form": form})
 
 
-
 class BorrarFamiliar(View):
-    template_name = "ejemplo/familiares.html"
-    initial = {"nombre":"", "direccion":"", "numero_pasaporte":""}
-    
-    def get(self, request, pk): 
+  template_name = 'ejemplo/familiares.html'
+  
+  def get(self, request, pk): 
       familiar = get_object_or_404(Familiar, pk=pk)
       familiar.delete()
       familiares = Familiar.objects.all()
-      return render(request, self.template_name, {'lista_familiares': familiar})      
+      return render(request, self.template_name, {'lista_familiares': familiares})
 
 
 class FamiliarList(ListView):
-  model = Familiar 
+    model = Familiar
 
 class FamiliarCrear(CreateView):
     model = Familiar
-    success_url = "/panel-familiar"
-    fields = ['nombre', 'direccion', 'numero_pasaporte']  
-
+    success_url = "/panel-familia"
+    fields = ["nombre", "direccion", "numero_pasaporte"]
 
 class FamiliarBorrar(DeleteView):
-  model = Familiar
-  success_url = "/panel-familia"
+    model = Familiar
+    success_url = "/panel-familia"
 
 class FamiliarActualizar(UpdateView):
-  model = Familiar
-  success_url = "/panel-familia"
-  fields = ["nombre", "direccion", "numero_pasaporte"]  
+    model = Familiar
+    success_url = "/success_updated_message"
+    fields = ["nombre", "direccion", "numero_pasaporte"]
+
+class FamiliarDetalle(DetailView):
+    model = Familiar
